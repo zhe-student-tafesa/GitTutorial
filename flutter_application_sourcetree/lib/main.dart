60e39fa20bdf89979dcaae878ce723795b838432
frankzhang2022 <frank.zhang@pixelforce.com.au>
2022-05-29 21:37:38 +0930

    main branch

--- flutter_application_sourcetree/lib/main.dart
+++ flutter_application_sourcetree/lib/main.dart
@@ -64,6 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     //setState时 自动重新build
+    //add cube
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
